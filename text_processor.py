5912b77236d602b386b13e1cc5fc175044ad7c43
Artem <kot7672@gmail.com>
2019-12-10 04:23:07 +0300

    Digits and underscore symbol included

--- text_processor.py
+++ text_processor.py
@@ -1,7 +1,7 @@
 import re
 
 class TextProcessor:
-    def __init__(self, pattern='[A-Za-zА-Яа-я]+'):
+    def __init__(self, pattern='[A-Za-zА-Яа-я0-9_]+'):
         self.pattern_re = re.compile(pattern)
     
     def is_word(self, word: str) -> bool:
